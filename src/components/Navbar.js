27c28bafecfc829004685abcff8516ff8b7b5890
mwhiatt <mwhiatt@purdue.edu>
2023-06-18 18:53:35 -0500

    setup homepage

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <header className="navbar">
+            <h2>Home</h2>
+            <h2>About</h2>
+            <h2>Education</h2>
+            <h2>Experience</h2>
+        </header>
+    )
+}
